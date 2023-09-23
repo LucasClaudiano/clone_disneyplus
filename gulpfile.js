f1d105598f5fc33f85721a31975b4d009bb4dd06
Lucas Matias <lucas_matias.13@hotmail.com>
2023-09-23 15:43:46 -0300

    Adicionando a estrutura hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'); 
-const sass = require('gulp-sass')(require('sass'))
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){ //compressão e compilação do SASS
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,14 @@ function styles(){ //compressão e compilação do SASS
         .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = styles;
+function images(){ //compressão das imagens do projeto
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch=function(){ //função responsável por fazer o monitoramento das mudanças e o run build automaticamente.
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
 }
